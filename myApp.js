3357c1c43008e72dc6a62a58666faf6e14e5cf60
JakeMartin1 <113862958+JakeMartin1@users.noreply.github.com>
2023-02-07 20:59:35 -0600

    working with middleware

--- myApp.js
+++ myApp.js
@@ -35,6 +35,18 @@ function (reg, res) {
 //use app.sendFile(), which requires an absolute path
 //absolutePath = __dirname + realativePath/file.ext
 
+app.use(logMiddleware); 
+//want middleware to log regardless of request method
+
+//need this to come first because it needs to apply to all requests
+
+//serve static assets with app.USE() & express.STATIC()
+//ex of static assests: css, stylesheets, imgs, etc
+const pathToAssets = __dirname + "/public"; //path on my server
+app.use("/public", express.static(pathToAssets)); //specifying paths for client requests
+
+
+
 const pathToIndex = __dirname + "/views/index.html";
 app.get("/", (req, res) => res.sendFile(pathToIndex));
 
@@ -61,11 +73,6 @@ app.get("/redorblue", (req, res) => {
 });
 
 
-//serve static assets with app.USE() & express.STATIC()
-//ex of static assests: css, stylesheets, imgs, etc
-const pathToAssets = __dirname + "/public"; //path on my server
-app.use("/public", express.static(pathToAssets)); //specifying paths for client requests
-
 app.get("/json", (req,res) => {
   let message = 'hello json';
   let name = 'jake';
@@ -83,6 +90,44 @@ app.get("/json", (req,res) => {
   
   res.json(jsonObj)
 });
+
+//authentication snytax
+app.get("/users/:username" , authMiddleware , (req,res) => {
+  const username = req.params.username;
+  res.send(`Hello, ${username}!`);
+});
+
+//middleware snytax
+/* 
+req = request object ; res = response object ; next = the next
+function in the middleware pipeline
+function someMiddleware(req, res, next) {
+  //some code that does stuff
+  next();
+}
+
+*/
+//middleware logger
+//log message to console for every request
+//format: "{method} {path} - {ip}"
+//access using req.ethod, req.path & req.ip
+
+
+function logMiddleware( req , res , next ) {
+  console.log(`${req.method} request was made to ${req.path} by IP: ${req.ip}`)
+  next();
+};
+
+//authentication middleware
+function authMiddleware(req,res,next) {
+  const token = req.header("Authorization");
+  // if (token == "secret-token") {
+  if ( 5 == 5){
+    next();
+  } else {
+    res.status(401).send("Unauthorized")
+  };
+};
 
 
 module.exports = app;
